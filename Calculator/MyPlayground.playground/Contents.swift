e268210850874f051c2724bc17d314828446f39f
Yogesh Darji <yogesh_darji99@yahoo.com>
2017-07-11 21:26:21 -0500

    Committing functions

--- Calculator/MyPlayground.playground/Contents.swift
+++ Calculator/MyPlayground.playground/Contents.swift
@@ -128,9 +128,26 @@ func makeIncrementer() -> ((Int) -> Int){
     increment(7)
 
 
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 7, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
 
 
 
+numbers.map({ (number: Int) -> Int in
+    let result = 3 * number
+    return result
+})
 
 
 
